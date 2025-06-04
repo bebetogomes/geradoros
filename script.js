b09472e6dd0d3eec8185dc64c44368ef356cb5f1
bebetogomes <bebetogomes2014@gmail.com>
2025-06-04 19:37:11 -0300

    editado nome

--- script.js
+++ script.js
@@ -41,12 +41,22 @@ function gerarPDF() {
     const element = document.getElementById('printArea');
     const empresa = document.getElementById('empresa').value || 'Empresa';
     const data = formatarData(document.getElementById('data').value);
+    const passageiro = document.getElementById('passageiro').value;
+    
     const opt = {
-        margin: 1,
-        filename: `OS_Transporte_${empresa}_${data}.pdf`,
+        margin: [10, 10, 10, 10],
+        filename: `OS_Transporte_${empresa}_${passageiro}_${data}.pdf`,
         image: { type: 'jpeg', quality: 0.98 },
-        html2canvas: { scale: 2 },
-        jsPDF: { unit: 'cm', format: 'a4', orientation: 'portrait' }
+        html2canvas: { 
+            scale: 2,
+            useCORS: true,
+            logging: false
+        },
+        jsPDF: { 
+            unit: 'mm', 
+            format: 'a4', 
+            orientation: 'portrait'
+        }
     };
 
     // Gerar PDF
